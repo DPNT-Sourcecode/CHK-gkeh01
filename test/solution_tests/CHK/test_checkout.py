af94dfa979f21d92884e56dfecdc8373edef6fb7
sbx_user1051 <sbx_user1051@169.254.175.93>
2023-08-17 19:17:19 +0000

    Thu Aug 17 19:17:19 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,3 +4,6 @@ from solutions.CHK import checkout_solution
 class TestCheckout():
     def test_checkout(self):
         assert checkout_solution.checkout("A A A A B") == 210
+
+    def test_wrong_input(self):
+        assert checkout_solution.checkout("A A A A J B") == -1        

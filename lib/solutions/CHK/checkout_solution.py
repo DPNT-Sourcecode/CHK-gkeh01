59749f9d52702331026b83c9ea4a2dd9fd9ccf77
sbx_user1051 <sbx_user1051@169.254.175.93>
2023-08-17 19:15:34 +0000

    Thu Aug 17 19:15:34 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,57 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    skus = skus.split(" ")
+    pricing = Pricing()
+    basket = Basket(skus)
+    return pricing.get_price(basket.items)
+
+
+class Basket:
+    def __init__(self, skus):
+        self.items = {}
+        for sku in skus:
+            if sku not in self.items:
+                self.items[sku] = 1
+            else:
+                self.items[sku] += 1
+
+
+class Pricing:
+    def __init__(self):
+        self.prices = {
+            'A': 50,
+            'B': 30,
+            'C': 20,
+            'D': 15
+        }
+        self.promotions = {
+            'A': {3: 130},
+            'B': {2: 45},
+        }
+    
+    def get_price(self, items):
+        total = 0
+        for sku, quantity in items.items():
+            item_price = self.get_price_for_item(sku, quantity)
+            if item_price == -1:
+                return -1
+            total += item_price
+        return total      
+
+
+    def get_price_for_item(self, sku, quantity):
+        if sku not in self.prices:
+            return -1
+        total = 0
+        while quantity > 0:
+            if sku in self.promotions:
+                for amount, price in self.promotions[sku].items():
+                    if amount <= quantity:
+                        total += price
+                        quantity -= amount
+            if quantity > 0:
+                total += self.prices[sku]
+                quantity -= 1
+            
+        return total      

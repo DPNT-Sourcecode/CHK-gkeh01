5092e66e1e9f404ec41cbe8ac7508397d0c3048e
sbx_user1051 <sbx_user1051@169.254.175.93>
2023-08-17 20:18:26 +0000

    Thu Aug 17 20:18:26 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -6,11 +6,14 @@ class TestCheckout():
         assert checkout_solution.checkout("AAAAB") == 210
         assert checkout_solution.checkout("AAAAAA") == 250
 
+
     def test_wrong_input(self):
         assert checkout_solution.checkout("AAAAJB") == -1
 
     def test_cross_promotion(self):
         assert checkout_solution.checkout("BBBEEEEE") == 230
+        assert checkout_solution.checkout("EEEEBB") == 160
+        assert checkout_solution.checkout("BEBEEE") == 160        
 
     def test_free_items(self):
         assert checkout_solution.checkout("FFF") == 20
